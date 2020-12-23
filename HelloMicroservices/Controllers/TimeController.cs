6c625d7eb541ba930f0e6e5729c38c8cfef7eae1
Greg Eakin <gregory.eakin@gmail.com>
2020-12-22 17:11:24 -0800

    Add the time controller

---
+++ HelloMicroservices/Controllers/TimeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace HelloMicroservices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly ILogger<TimeController> _logger;
+
+        public TimeController(ILogger<TimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<string> Get()
+        {
+            return DateTime.UtcNow.ToLongDateString();
+        }
+    }
+}
