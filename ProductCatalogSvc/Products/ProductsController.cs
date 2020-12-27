8eae3bb6aeb98d730500558cbb9e8af17d3a29b1
Greg Eakin <gregory.eakin@gmail.com>
2020-12-26 23:56:00 -0800

    Remove links

--- ProductCatalogSvc/Products/ProductsController.cs
+++ ProductCatalogSvc/Products/ProductsController.cs
@@ -19,8 +19,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ProductCatalogSvc.Products
 {
     [Route("api/[controller]")]
--- ShoppingCartSvc/EventFeed/EventsController.cs
+++ ShoppingCartSvc/EventFeed/EventsController.cs
@@ -18,8 +18,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ShoppingCartSvc.EventFeed
 {
     [Route("api/[controller]")]
